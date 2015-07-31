527dec05829b1f26a2e523a3432fc1115858cc4e
Dylan Wang <dylan.wang@kitware.com>
2015-07-31 08:37:46 -0400

    Add new tests for exporters

---
+++ Source/Applications/ModelBuilder/Testing/Python/RunModelBuilderAndVerify.py
@@ -0,0 +1,60 @@
+
+# This script runs ModelBuilder (assuming it produces something)
+# and then a python verification script (calls a function called "test")
+
+import sys
+from subprocess import call, check_call
+from pprint import pprint
+
+def RunMBAndVerify():
+
+	if len(sys.argv) != 7:
+		print("Incorrect Number of arguments. Got %i expected %i" % (len(sys.argv), 7))
+		print("Format: %s ModelBuilderPath testdir testScript pythonExecutable verifyScript testDataRoot" % (sys.argv[0]))
+		pprint("Got: %s" % sys.argv)
+		return 1
+
+	pprint(sys.argv)
+
+	# sys.argv[0] is the script (RunModelBuilderAndVerify)
+	modelBuilder = sys.argv[1]
+	testdir = sys.argv[2]
+	testScript = sys.argv[3]
+	pythonExecutable = sys.argv[4]
+	verifyScript = sys.argv[5]
+	testDataRoot = sys.argv[6]
+
+	status = 0
+
+	# run ModelBuilder
+	try:
+		c = [modelBuilder, "-dr", "--test-directory=%s" % testdir, "--test-script=%s" % testScript, "--exit"]
+		print("Trying call: %s" % c)
+		status = call(c)
+	except Exception, e:
+		print("ModelBuilder call failed: %s" % e)
+		status = -1
+		return status
+
+	print("ModelBuilder export: %i. Now verifying" % (status))
+
+	# run verify script
+	# note: we use the command-line call rather than import because
+	# 	import-by-filename is depreciated in 3.4
+	try:
+		c = [pythonExecutable, verifyScript, testDataRoot, testScript, testdir]
+		# print("Trying call: %s" % c)
+		r = check_call(c)
+		print("Verification done: %i" % r)
+		status = status + r
+	except Exception, e:
+		print('Verify script call failed: %s' % e)
+		return -2
+
+	print('Done. Status: %i' % status)
+
+	return status
+
+if __name__ == '__main__':
+	sys.exit(RunMBAndVerify())
+	
---
+++ Source/Applications/ModelBuilder/Testing/Python/VerifyAttributeSave.py
@@ -0,0 +1,117 @@
+import os
+import sys
+from pprint import pprint
+import smtk
+from smtk.simple import *
+
+def verifyAssociation():
+    mgr = smtk.model.Manager.create()
+    sess = mgr.createSession('discrete')
+    sess.assignDefaultName()
+    SetActiveSession(sess)
+
+    sref = GetActiveSession()
+    r = sref.op('read')
+
+    if len(sys.argv) < 4:
+        print("Wrong number of arguments %i should be %i" % (len(sys.argv), 4))
+        return
+    elif len(sys.argv) > 4:
+        print("Note: only 4 arguments used")
+
+    try:
+        datadir = sys.argv[1]
+        print("datadir=%s" % datadir)
+        modelpath = os.path.join(datadir,'2D-CMBModels')
+        modelpath = os.path.join(modelpath, 'test2D.cmb')
+    except:
+        print 'Failed to determine model path'
+        return -1
+
+    print "modelpath", modelpath
+    model = smtk.model.Model(Read(modelpath)[0])
+    
+    # read the model tree
+    faces = model.cells() # list of faces
+    edges = []
+    print('Faces:')
+    for x in model.cells(): # faces
+        print x.name(), x.entity()
+        for y in x.boundingCells(): #edges
+            edges.append(y)
+            # print ' ', y.name(), y.entity()
+            for z in y.boundingCells(): #vertices
+                # print '  ', z.name(), z.entity()
+                pass # if ever testing vertex association, add them here  
+
+    # Get the resource for the CRF
+    print 'CMB:'
+    rs = getCRF()
+    if rs == None:
+        return 2
+
+    # rs now contains the CRF
+    resource = rs.get('simbuilder')
+    attSystem = smtk.attribute.System.CastTo(resource)
+    attributeTypes = attSystem.definitions()
+
+    # list of tuples with attribute instance names and entity names
+    matches = list()
+
+    print "attributeTypes:"
+    for aType in attributeTypes:
+        defs = attSystem.findAttributes(aType.type())
+        print aType.type()
+        for attribute in defs:
+            print " ", attribute.name(), "| Associations:", attribute.associations()
+            if attribute.associations() is None:
+                continue
+            for uuid in attribute.associatedModelEntityIds():
+                for face in faces:
+                    if uuid == face.entity():
+                        print "\tMatched", attribute.name(), "with", face.name()
+                        matches.append( (attribute.name(), face.name()) )
+                for edge in edges:
+                    if uuid == edge.entity():
+                        print "\tMatched", attribute.name(), "with", edge.name()
+                        matches.append( (attribute.name(), edge.name()) )
+
+    # verify the matches
+    matches = list(set(matches)) # remove duplicate matches
+
+    correctMatches = [ ('SolidMaterial-0', 'Face1'), ('SolidMaterial-0', 'Face4'), ('SolidMaterial-1', 'Face3'), ('VelocityBound-0', 'Edge4'), ('VelocityBound-0', 'Edge7') ]
+    for m in correctMatches:
+        if m not in matches:
+            print m, "match not found"
+            return -2
+    if len(correctMatches) != len(matches):
+        print "Expected", len(correctMatches), "matches, got", len(matches)
+        print "Matches found:"
+        for match in matches:
+            print match
+        return -3
+
+
+    return 0
+
+def getCRF():
+    rs = smtk.common.ResourceSet()
+    reader = smtk.io.ResourceSetReader()
+    logger = smtk.io.Logger()
+
+    datadir = sys.argv[3]
+    crfpath = os.path.join(datadir, 'VerifyAttributeSave.crf')
+    print('crfpath: %s' % crfpath)
+    if reader.readFile(crfpath, rs, logger) == True:
+        print('Error reading file ' + logger.convertToString())
+        return None
+
+    #file successfully read...
+    return rs
+
+def test():
+    print("In Test")
+    return verifyAssociation()
+
+if __name__ == '__main__':
+    sys.exit(test())
