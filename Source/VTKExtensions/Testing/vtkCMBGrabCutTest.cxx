897fcf55a660a927b3d709c33683f79be654e23e
Jacob Becker <jacob.becker@kitware.com>
2016-08-19 16:43:39 -0400

    add watershed

--- Source/VTKExtensions/Testing/vtkCMBGrabCutTest.cxx
+++ Source/VTKExtensions/Testing/vtkCMBGrabCutTest.cxx
@@ -10,6 +10,7 @@
 
 #include "vtkSmartPointer.h"
 #include "vtkCMBGrabCutFilter.h"
+#include "vtkCMBWatershedFilter.h"
 #include "vtkTesting.h"
 #include "vtkNew.h"
 #include "vtkPNGReader.h"
@@ -23,20 +24,21 @@ int main(int argc, char** argv)
   mask_reader->SetFileName("/Users/jacobbecker/Downloads/World_Imagery_mask.png");
   vtkSmartPointer<vtkGDALRasterReader> gdal_reader = vtkSmartPointer<vtkGDALRasterReader>::New();
   gdal_reader->SetFileName("/Users/jacobbecker/Downloads/World Imagery.tif");
-  vtkSmartPointer<vtkCMBGrabCutFilter> grabCut = vtkSmartPointer<vtkCMBGrabCutFilter>::New();
-  grabCut->SetInputConnection(0, gdal_reader->GetOutputPort());
-  grabCut->SetInputConnection(1, mask_reader->GetOutputPort());
-  grabCut->SetNumberOfIterations(24);
+  //vtkSmartPointer<vtkCMBGrabCutFilter> filter = vtkSmartPointer<vtkCMBGrabCutFilter>::New();
+  vtkSmartPointer<vtkCMBWatershedFilter> filter = vtkSmartPointer<vtkCMBWatershedFilter>::New();
+  filter->SetInputConnection(0, gdal_reader->GetOutputPort());
+  filter->SetInputConnection(1, mask_reader->GetOutputPort());
+  //filter->SetNumberOfIterations(24);
   vtkSmartPointer<vtkXMLPolyDataWriter> writer =vtkSmartPointer<vtkXMLPolyDataWriter>::New();
   writer->SetFileName("/Users/jacobbecker/Downloads/testWorld.vtp");
-  writer->SetInputConnection(grabCut->GetOutputPort(2));
+  writer->SetInputConnection(filter->GetOutputPort(2));
   vtkSmartPointer<vtkPNGWriter> writer_label = vtkSmartPointer<vtkPNGWriter>::New();
   writer_label->SetFileName("/Users/jacobbecker/Downloads/testWorldLabel.png");
-  writer_label->SetInputConnection(grabCut->GetOutputPort(0));
+  writer_label->SetInputConnection(filter->GetOutputPort(0));
   writer_label->Write();
   vtkSmartPointer<vtkPNGWriter> writer_next = vtkSmartPointer<vtkPNGWriter>::New();
   writer_next->SetFileName("/Users/jacobbecker/Downloads/testWorldNext.png");
-  writer_next->SetInputConnection(grabCut->GetOutputPort(1));
+  writer_next->SetInputConnection(filter->GetOutputPort(1));
   writer_next->Write();
   writer->Update();
 
