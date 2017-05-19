81f74be03bf27ab87cb8fd1a4c6b65e9ac8730b6
Alvaro Sanchez <alvaro.sanchez@kitware.com>
2017-05-19 14:03:58 -0400

    Customized help menu in ModelBuilder.
    PDF user guides for CMB and SMTK are now exposed.  The legacy help
    dialog is for the moment hidden.
    
    Added pdf documentation for apple installs.

--- Source/CMake/CMBInstallApp.cmake
+++ Source/CMake/CMBInstallApp.cmake
@@ -5,12 +5,37 @@ if (NOT APPLE)
   return()
 endif()
 
+# Download PDF documentation. These are used in ModelBuilder's help menu.
+macro(download_pdf_guides)
+  set (CMB_PDF_DOC_DIR "${CMAKE_BINARY_DIR}/downloads/pdf-doc")
+  file(DOWNLOAD "https://media.readthedocs.org/pdf/cmb/master/cmb.pdf"
+     "${CMB_PDF_DOC_DIR}/CMBUsersGuide.pdf"
+    INACTIVITY_TIMEOUT 10
+    SHOW_PROGRESS)
+
+  file(DOWNLOAD "https://media.readthedocs.org/pdf/smtk/latest/smtk.pdf"
+     "${CMB_PDF_DOC_DIR}/SMTKUsersGuide.pdf"
+    INACTIVITY_TIMEOUT 10
+    SHOW_PROGRESS)
+endmacro()
+
 # cleanup_bundle is called to fillup the .app file with libraries and plugins
 # built. This does not package any external libraries, only those that are
 # at the specified locations. This will create a workable bundle that works on
 # the machine where its built (since it relies on other shared frameworks and
 # libraries) e.g. python
 macro(cleanup_bundle app app_root libdir)
+  # Install PDF documentation
+  download_pdf_guides()
+  file(GLOB guides "${CMB_PDF_DOC_DIR}/*")
+  foreach(guide IN LISTS guides)
+    if(EXISTS "${guide}" AND NOT IS_DIRECTORY "${guide}")
+      file(INSTALL "${guide}"
+        DESTINATION "${app_root}/Contents/doc"
+        USE_SOURCE_PERMISSIONS)
+    endif()
+  endforeach()
+
   # take all libs from ${ARGN} and put it in the Libraries dir.
   file(GLOB_RECURSE dylibs ${libdir}/*.dylib)
   file(GLOB_RECURSE solibs ${libdir}/*.so)
